9fd20c2b3311c498d38b4f95b1dc8add83c83b39
hari-testpress <hari@testpress.in>
2023-05-23 11:41:47 +0530

    Initial Commit

---
+++ playerWithSwiftUI/playerWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  playerWithSwiftUIApp.swift
+//  playerWithSwiftUI
+//
+//  Created by Testpress on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct playerWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
