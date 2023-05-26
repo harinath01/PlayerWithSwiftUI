de68d0dd14ca66c1304a1e321ef6a4839097e413
hari-testpress <hari@testpress.in>
2023-05-26 14:57:57 +0530

    Refactor the code

--- playerWithSwiftUI/Views/VideoPlaybackView.swift
+++ playerWithSwiftUI/Views/VideoPlaybackView.swift
@@ -11,47 +11,16 @@ class VideoPlaybackUIView: UIView {
         set { playerLayer.player = newValue }
     }
     private var playerLayer: AVPlayerLayer { layer as! AVPlayerLayer }
-    private var orgCode: String!
-    private var videoId: String!
-    private var accessToken: String!
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    init(orgCode: String!, videoId: String!, accessToken: String!){
-        super.init(frame: .zero)
-        self.orgCode = orgCode
-        self.videoId = videoId
-        self.accessToken = accessToken
-        self.backgroundColor = .black
-        self.configurePlayerWithVideo()
-    }
-    
-    private func configurePlayerWithVideo(){
-        StreamsAPIClient.fetchVideo(orgCode: orgCode, videoId: videoId, accessToken: accessToken){ videoDetails, error in
-            if let videoDetails = videoDetails {
-                self.player = AVPlayer(url: URL(string: videoDetails.playbackURL)!)
-                self.player?.play()
-            } else if let error = error {
-                debugPrint(error.localizedDescription)
-            }
-        }
-    }
 }
 
 
 struct VideoPlaybackView: UIViewRepresentable {
-    var orgCode: String
-    var videoId: String
-    var accessToken: String
+    var player: AVPlayer
     
     func makeUIView(context: Context) -> VideoPlaybackUIView {
-        return VideoPlaybackUIView(
-            orgCode: orgCode,
-            videoId: videoId,
-            accessToken: accessToken
-        )
+        let videoPlaybackView = VideoPlaybackUIView()
+        videoPlaybackView.player = self.player
+        return videoPlaybackView
     }
     
     func updateUIView(_ uiView: VideoPlaybackUIView, context: Context) { }
--- playerWithSwiftUI/Views/VideoPlayer.swift
+++ playerWithSwiftUI/Views/VideoPlayer.swift
@@ -1,15 +1,37 @@
 import SwiftUI
+import AVKit
 
 struct VideoPlayer: View {
     var orgCode: String
     var videoId: String
     var accessToken: String
+    @State private var player: AVPlayer?
+    
+    init(orgCode: String, videoId: String, accessToken: String) {
+        self.orgCode = orgCode
+        self.videoId = videoId
+        self.accessToken = accessToken
+    }
+    
+    private func configurePlayerWithVideo() {
+        StreamsAPIClient.fetchVideo(orgCode: orgCode, videoId: videoId, accessToken: accessToken) { videoDetails, error in
+            if let videoDetails = videoDetails {
+                self.player = AVPlayer(url: URL(string: videoDetails.playbackURL)!)
+                self.player?.play()
+            } else if let error = error {
+                debugPrint(error.localizedDescription)
+            }
+        }
+    }
     
     var body: some View {
         ZStack {
-            VideoPlaybackView(orgCode: orgCode, videoId: videoId, accessToken: accessToken)
+            if let player = player {
+                VideoPlaybackView(player: player)
+            }
             ControlsView()
         }
+        .onAppear(perform: configurePlayerWithVideo)
         .background(Color.black)
     }
 }
