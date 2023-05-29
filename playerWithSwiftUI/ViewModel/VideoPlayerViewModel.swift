1378b03c0a1c2a0569626d7e60337df5220e3412
hari-testpress <hari@testpress.in>
2023-05-29 20:38:03 +0530

    fix typo

--- playerWithSwiftUI/ViewModel/VideoPlayerViewModel.swift
+++ playerWithSwiftUI/ViewModel/VideoPlayerViewModel.swift
@@ -42,7 +42,7 @@ class VideoPlayerViewModel: NSObject, ObservableObject {
     
     private func addObservers(){
         addPlayerTimeObserver()
-        addPlayerTimeObserver()
+        addPlayerBufferingObservers()
         addPlayerStatusObserver()
     }
     
