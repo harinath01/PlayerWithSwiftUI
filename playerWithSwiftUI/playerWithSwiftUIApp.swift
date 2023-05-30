c2be409b07c873e7c5190f9b18c43c5d2419e6f9
hari-testpress <hari@testpress.in>
2023-05-30 11:09:26 +0530

    Remove appdelegate from main

--- playerWithSwiftUI/playerWithSwiftUIApp.swift
+++ playerWithSwiftUI/playerWithSwiftUIApp.swift
@@ -1,23 +1,8 @@
 import SwiftUI
 import AVKit
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let audioSession = AVAudioSession.sharedInstance()
-        do {
-            try audioSession.setCategory(AVAudioSession.Category.playback)
-        } catch {
-            debugPrint("Setting category to AVAudioSession.Category.playback failed.")
-        }
-        return true
-    }
-}
-
-
 @main
 struct main: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    
     var body: some Scene {
         WindowGroup {
             VStack{
